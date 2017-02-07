d5b06ca0aceae3870a149a89a524cb7f12e91194
GloomySunday <121055230@qq.com>
2017-02-07 15:46:03 +0800

    initial

---
+++ socketSwift/socketSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  socketSwift
+//
+//  Created by 孟钰丰 on 2017/2/7.
+//  Copyright © 2017年 GloomySunday. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
